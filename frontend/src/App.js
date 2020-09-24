b2467600ed3e678d322b44f216ff519cf328605d
mateus.cunha <mateus.cunha@ccc.ufcg.edu.br>
2020-09-23 22:29:42 -0300

    inicialização do design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu } from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+  AppstoreOutlined,
+  TrophyOutlined,
+  InfoCircleOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content } = Layout;
 
 function App() {
+  // const [collapsed, setCollapsed] = useState(false);
+
+  useEffect(() => {
+    console.log('atualizou');
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout>
+        <Header className='header'>
+          <div className='logo' />
+          <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
+            <Menu.Item icon={<AppstoreOutlined />} key='1'>
+              Início
+            </Menu.Item>
+            <Menu.Item icon={<TrophyOutlined />} key='2'>
+              Ranking
+            </Menu.Item>
+            <Menu.Item icon={<InfoCircleOutlined />} key='3'>
+              Sobre
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content className='content'>
+          <h2>Content</h2>
+        </Content>
+      </Layout>
     </div>
   );
 }
