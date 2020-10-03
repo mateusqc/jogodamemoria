e12f7425edd9996bde00d23078a33888029ce75b
mateus.cunha <mateus.cunha@ccc.ufcg.edu.br>
2020-10-03 13:05:48 -0300

    hotfix/org_diretorios

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var hiscoresRouter = require('./routes/hiscores');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
+var hiscoresRouter = require('./src/routes/hiscores');
 
 var app = express();
 
