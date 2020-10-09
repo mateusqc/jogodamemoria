8a99916ce19c56f88eaef7f3b1449a79c7b1af1c
mateus.cunha <mateus.cunha@ccc.ufcg.edu.br>
2020-10-09 02:52:13 -0300

    hotfix/removendo tabuleiro mockado

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,15 +2,8 @@ import { Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
 import Board from '../components/Board';
 
-const initBoardMatrix = [
-  [0, 2, 3, 5],
-  [3, 1, 4, 5],
-  [1, 4, 2, 6],
-  [0, 6, 7, 7],
-];
-
 function Index() {
-  const [boardMatrix, setBoardMatrix] = useState(initBoardMatrix);
+  const [boardMatrix, setBoardMatrix] = useState([[]]);
   const [loading, setLoading] = useState(false);
   const [page] = useState();
 
